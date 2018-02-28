c709300be2c701f017b28e1a90fe99a0da29f431
pardeepsingh12365 <29483354+pardeepsingh12365@users.noreply.github.com>
2018-02-28 14:46:39 +0530

    Update pkbot.js

--- pkbot.js
+++ pkbot.js
@@ -71,7 +71,7 @@ bot.on("message", async(message) => {
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
     if(command == "gsearch" || command === "google" || command === "g") {
-        let args3 = message.content.substring(command.length + 3);
+        let args3 = message.content.substring(command.length + 2);
         let searchMessage = await message.reply('Searching... Sec.');
         let searchUrl = `https://www.google.com/search?q=${encodeURIComponent(args3)}`;
         message.channel.send(args3);
