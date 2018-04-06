4831da8149f2ac08a068ca7c1a472354ce6c820e
pardeepsingh12365 <29483354+pardeepsingh12365@users.noreply.github.com>
2018-04-06 21:44:09 +0530

    Update pkbot.js

--- pkbot.js
+++ pkbot.js
@@ -245,6 +245,7 @@ bot.on("message", async(message) => {
         let args3 = message.content.substring(command.length + 2);
         let searchMessage = await message.reply('Searching... Sec.');
         let searchUrl = `https://www.google.com/search?q=${encodeURIComponent(args3)}`;
+        message.channel.send(`'${searchUrl}'`)
         return snekfetch.get(searchUrl).then((result) => {
             let $ = cheerio.load(result.text);
             let googleData = $('.r').first().find('a').first().attr('href');
